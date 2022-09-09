b6e2f4da6cbc824498336c32b907cc536bae58ed
adesh <adesh@example.com>
2022-09-09 12:47:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1870px",
+      },
+      fontFamily: {
+        Pmarks: ["Permanent Marker", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
